40473dfc3ef686215908e915cfa99ba1b1dfd26f
Mehmet Akif Duran <mehmetakifduranngithub@gmail.com>
2025-11-22 17:07:56 +0300

    Add files via upload

--- FtpWebApiProject/program.cs
+++ FtpWebApiProject/program.cs
@@ -2,6 +2,18 @@ using FtpWebApiProject.Services;
 
 var builder = WebApplication.CreateBuilder(args);
 
+// 1. CORS Servisini Ekliyoruz (GitHub Pages'ın erişebilmesi için şart)
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy("AllowAll",
+        builder =>
+        {
+            builder.AllowAnyOrigin()  // Kim gelirse gelsin kabul et (GitHub Pages vs.)
+                   .AllowAnyMethod()  // GET, POST, PUT hepsine izin ver
+                   .AllowAnyHeader(); // Tüm başlıklara izin ver
+        });
+});
+
 // Servisleri ekle
 builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
@@ -14,14 +26,17 @@ builder.Services.AddScoped<IFtpService, FtpService>();
 
 var app = builder.Build();
 
-// ... kodun geri kalanı (Swagger, HttpsRedirection vs.)
-if (app.Environment.IsDevelopment())
-{
-    app.UseSwagger();
-    app.UseSwaggerUI();
-}
+// 2. Swagger Ayarı (if koşulunu kaldırdık ki Render'da da açılsın)
+app.UseSwagger();
+app.UseSwaggerUI();
 
 app.UseHttpsRedirection();
+
+// 3. CORS Politikasını Devreye Al (Tam buraya yazılmalı!)
+app.UseCors("AllowAll");
+
 app.UseAuthorization();
+
 app.MapControllers();
+
 app.Run();
